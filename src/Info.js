24607fcc60877cc5309de4f1251cbf28544aa9a9
Juan C Hernandez <JChern83@users.noreply.github.com>
2020-03-19 16:08:16 -0700

    Day2 added styled components

--- src/Info.js
+++ src/Info.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import {Badge, Card, CardTitle, CardSubtitle, CardText, Container} from "reactstrap";
+import styled from "styled-components";
+
+const Ptext = styled.p `
+font-size: 15px;
+color: palevioletred;
+background: papayawhip;
+
+
+`;
 
 const Info =({title, date, explanation, copyright})=>{
+
     return (
       <Container fluid ="md">
     <Card body>
       <div className="photo-info">
         <CardTitle><Badge color = "success" ><p>{date}</p></Badge></CardTitle>
         <CardSubtitle><Badge color = "secondary"><h2>{title}</h2></Badge></CardSubtitle>
-        <CardText className="text-center" ><p>{explanation}</p></CardText>
+        <Ptext>{explanation}</Ptext>
       </div>
       </Card>
       </Container>

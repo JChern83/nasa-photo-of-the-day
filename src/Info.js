6b3529d81eabff7c5f0320db0741875b42f21968
Juan C Hernandez <JChern83@users.noreply.github.com>
2020-03-19 15:50:49 -0700

    Day2 More styles added

--- src/Info.js
+++ src/Info.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
-import {Badge, Card, CardTitle, CardSubtitle, CardText} from "reactstrap";
+import {Badge, Card, CardTitle, CardSubtitle, CardText, Container} from "reactstrap";
 
 const Info =({title, date, explanation, copyright})=>{
     return (
+      <Container fluid ="md">
     <Card body>
       <div className="photo-info">
-        <CardTitle><Badge color = "primary" ><p>{date}</p></Badge></CardTitle>
-        <CardSubtitle><h2>{title}</h2></CardSubtitle>
+        <CardTitle><Badge color = "success" ><p>{date}</p></Badge></CardTitle>
+        <CardSubtitle><Badge color = "secondary"><h2>{title}</h2></Badge></CardSubtitle>
         <CardText className="text-center" ><p>{explanation}</p></CardText>
       </div>
       </Card>
+      </Container>
     );
   };
 export default Info; 
